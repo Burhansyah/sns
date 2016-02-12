1810915b89ec12083953a33b7b71389873493035
Dmitry Kunin <dkunin@avito.ru>
2016-02-12 11:32:13 +0300

    Added arguments parser and help

--- README.md
+++ README.md
@@ -10,11 +10,20 @@ Just in case you are too lazy to open your package.json file
 
 Navigate to your project directory, then run:
 
-      sns
-      ? Choose script to run (Use arrow keys)
-      ❯ command_of_your_script
+        Usage: sns [options]
+        Options:
+          -h, --help   Show help                                               [boolean]
+          -v, --vocal  Vocal, show description of task
+
+        sns
+        ? Choose script to run (Use arrow keys)
+        ❯ command_of_your_script
         command_of_your_script_2
 
 Select the script to run.
 
 Get some work done!
+
+## License
+
+MIT © [Dmitri Kunin](http://dkun.in)
--- index.js
+++ index.js
@@ -8,13 +8,29 @@ var path = require('path');
 var shell = require('shelljs');
 var packagejson = JSON.parse(fs.readFileSync(path.resolve('./package.json')).toString());
 
+var argv = require('yargs')
+    .usage('Usage: sns [options]')
+    .help('h')
+    .describe('v', 'Vocal, show description of task')
+    .alias('v', 'vocal')
+    .alias('h', 'help')
+    .epilogue('Happy running! Made by DKunin http://dkunin.github.io/').argv;
+ 
+console.log('Project: ' + packagejson.name);
+console.log('Description: ' + packagejson.description);
+
 inquirer.prompt([
   {
     type: 'list',
     name: 'script',
     message: 'Choose script to run',
-    choices: Object.keys(packagejson.scripts),
-    filter: function( val ) { return val.toLowerCase(); }
+    choices: Object.keys(packagejson.scripts).map(function(singleKey){
+        if(argv.v) {
+            return singleKey + ': ' + packagejson.scripts[singleKey];
+        }
+        return singleKey;
+    })
+    
   }
 ], function( answers ) {
     var command = ' npm run ' + answers.script;
